20e8419f6aff8361a0bf72509af3605a13797fb3
brian <brian@accordionhealth.com>
2016-08-28 12:31:07 -0500

    small code cleanup

--- myscript.js
+++ myscript.js
@@ -22,57 +22,70 @@ function m(src, which, docIndex) {
       linkText = linkText + '&i=' + which;
     }
   }
-  var theWindowSrc = getPopupWindowSrc(linkText, 'morph');
-  
-}
-
-function getPopupWindowSrc(linkText, name) {
-  $.get(linkText, function(data) {
-    var tmpHTML = $.parseHTML(data);
-    var objects = $(tmpHTML).find('.lemma');
-    for (var i = 0; i < objects.length; i++) {
-      console.log(objects);
-      var word = $(objects[i]).find('.lemma_header').find('h4').text();
-      console.log(word);
-      var definition = $(objects[i]).find('.lemma_definition').text();
-      console.log(definition);
-      var links = $(objects[i]).find('a');
-      console.log(links);
-      for (var j = 0; j < links.length; j++) {
-        console.log($(links[j]).text());
-        if ($(links[j]).text() == 'Lewis & Short') {
-          var linkItems = $(links[j]).attr('id').split(':');
-          console.log(linkItems);
-          var targetWord = linkItems[3].substring(6,linkItems[3].length);
-          console.log(targetWord);
-          targetWord = targetWord.substring(0, targetWord.length-5);
-          console.log(targetWord);
-          var dictEntryLink = "text?doc=" + linkItems[0] + "%3A" + linkItems[1] + "%3A" + linkItems[2] + "%3Aentry%3D" + targetWord;
-          $.get(dictEntryLink, function(newData) {
-            console.log(dictEntryLink);
-            var dictEntryHTML = $.parseHTML(newData);
-            var entryContainer = $(dictEntryHTML).find('#text_main');
-            var tmpContainer = $(entryContainer).find('.text')[0];
-            var grammarParts = $(tmpContainer).clone().children().remove().end().text();
-            var word = $($(tmpContainer).find('span.la')[0]).find('a').text();
-            var definitions = $(tmpContainer).find('.lex_sense');
-            console.log(word + ' ' + grammarParts);
-            var vocabHelp = document.getElementById('vocabHelp');
-            for (var k = definitions.length-1; k >= 0; k--) {
-              var tmpDefinition = document.createElement("p");
-              tmpDefinition.innerHTML = $(definitions[k]).text();
-              console.log(tmpDefinition);
-               
-              vocabHelp.insertBefore(tmpDefinition, vocabHelp.firstChild);
-            }
-            var wordText = document.createElement("p");
-            wordText.innerHTML = word + ' ' + grammarParts;
-            vocabHelp.insertBefore(wordText, vocabHelp.firstChild);
-          });
-        }
-      }
+  clearHelpBox();
+  getPopupWindowHTML(linkText, 'morph', function(theWindowSrc) {
+    var definitions = findDefinitions(theWindowSrc);
+    console.log(definitions);
+    for (var x = 0; x < definitions.length; x++) {
+      doDictSearch(definitions[x][2]); 
     }
   });
 }
 
-$('a').css("color", "blue");
+function clearHelpBox() {
+  var vocabHelp = document.getElementById('vocabHelp');
+  $(vocabHelp).empty();
+  var waitingText = document.createTextNode("Loading..."); 
+  vocabHelp.appendChild(waitingText);
+}
+
+function getPopupWindowHTML(linkText, name, callback) {
+  $.get(linkText, function(data) {
+    console.log('returning');
+    callback($.parseHTML(data));
+  });
+}
+
+function findDefinitions(srcPage) {
+  var definitions = [];
+  var definitionContainers = $(srcPage).find('.lemma');
+  for (var i = 0; i < definitionContainers.length; i++) {
+    var word = $(definitionContainers[i]).find('.lemma_header').find('h4').text();
+    var definition = $(definitionContainers[i]).find('.lemma_definition').text();
+    var links = $(definitionContainers[i]).find('a');
+    var definitionsItem = [word, definition, links];
+    definitions.push(definitionsItem);
+  }
+  return definitions;
+}
+
+function doDictSearch(links) {
+  var vocabHelp = document.getElementById('vocabHelp');
+  for (var j = 0; j < links.length; j++) {
+    if ($(links[j]).text() == 'Lewis & Short') {
+      var linkItems = $(links[j]).attr('id').split(':');
+      var targetWord = linkItems[3].substring(6,linkItems[3].length);
+      targetWord = targetWord.substring(0, targetWord.length-5);
+      var dictEntryLink = "text?doc=" + linkItems[0] + "%3A" + linkItems[1] + "%3A" + linkItems[2] + "%3Aentry%3D" + targetWord;
+      $.get(dictEntryLink, function(newData) {
+        console.log(dictEntryLink);
+        var dictEntryHTML = $.parseHTML(newData);
+        var entryContainer = $(dictEntryHTML).find('#text_main');
+        var tmpContainer = $(entryContainer).find('.text')[0];
+        var grammarParts = $(tmpContainer).clone().children().remove().end().text();
+        var word = $($(tmpContainer).find('span.la')[0]).find('a').text();
+        var definitions = $(tmpContainer).find('.lex_sense');
+        for (var k = definitions.length-1; k >= 0; k--) {
+          var tmpDefinition = document.createElement("p");
+          tmpDefinition.innerHTML = $(definitions[k]).text();
+          console.log(tmpDefinition);
+           
+          vocabHelp.insertBefore(tmpDefinition, vocabHelp.firstChild);
+        }
+        var wordText = document.createElement("p");
+        wordText.innerHTML = word + ' ' + grammarParts;
+        vocabHelp.insertBefore(wordText, vocabHelp.firstChild);
+      });
+    }
+  }
+}
